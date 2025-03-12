8c21cca91a3a313cb54874256d173c1b522678e1
Paulo Henrique Silva Junior <106300560+pauloh-02@users.noreply.github.com>
2025-03-12 19:32:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-/*
-let numero = 0, i 
-let resultado = ""
 
-numero = parseFloat(prompt("Digite um número que iremos multiplicar do 1 até o 20."))
-
-for(i = 1; i <= 20; i++){
-    resultado += numero + " x " + i + " = " + (numero * i) + "\n" 
-}
-
-alert(
-    "A tabuada do " + numero + " até o 20 é:\n" + resultado 
-)
-*/
 const palavra = prompt("Escreva uma palavra")
 let palavrainvertida
 
